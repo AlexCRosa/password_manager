dbe2c62c023119fb61c079f1119fcb23392515c8
Alex Cesar <alex_cesar20@hotmail.com>
2023-12-12 14:55:44 -0500

    Added flash messages feature

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
 
 @app.route('/')
 def index():
@@ -11,16 +12,17 @@ def process():
     try:
         input_data = int(request.form['input_data'])
     except ValueError:
-        return render_template('index.html', message="Please use only numbers.")
+        flash("Invalid input.")
+        return redirect('/')
 
     processed_result = your_python_function(input_data)
-    return render_template('result.html', result=processed_result)
+    return render_template('index.html', password=processed_result)
 
 def your_python_function(data):
     # Your Python logic here
     # Return the processed result
     data = data * data
-    return data
+    return f"The password generated is: {data}"
 
 if __name__ == '__main__':
     app.run(debug=True)
