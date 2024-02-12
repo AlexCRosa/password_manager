12c3d1bdde4c4be6363fed9b45d4e0060086d06a
Alex Cesar <alex_cesar20@hotmail.com>
2024-02-12 16:46:27 -0500

    Deactivated session 'remember-me' when the user login

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import Users
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db   # means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
@@ -17,7 +17,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Login Succesful.", "success")
-                login_user(user, remember=True)
+                login_user(user, remember=False, duration=None) # 'remember=True' = Keeps the user logged in even after the window is closed
                 return redirect(url_for('views.profile'))
             else:
                 flash("Incorrect password.", "warning")
